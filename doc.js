f8b1faa124520f7621a291bbb892f6e35770e117
iliajuso <iliajuso@gmail.com.com>
2023-04-14 23:50:48 +0400

    add corect project

--- doc.js
+++ doc.js
@@ -11,7 +11,7 @@ navigator.geolocation.getCurrentPosition(success, error);
  
  async function getWeather(){
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?lat=41.716667&lon=&units=metric&appid=06ecae34019f389faf0e8d8036fb3987`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=41.716667&lon=44.783333&units=metric&appid=1cd8c91ab257624a491a4e5e9eef35e0`;
      let weather = await fetch(url);
      let data = await weather.json()
       console.log(data)
@@ -37,20 +37,25 @@ getWeather()
     try{
       const weatherData= await getWeather()
       createWeather(weatherData)
-    }catch (eror){
-      console.log(eror)
+    }catch (error){
+      console.log(error)
       const location = await getCity(await getIp ())
       console.log(location)
       createWeather(location)
-    }
+   }
   }
- async function getCity(city) {
+
+async function getCity(city) {
+  try {
     let response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=06ecae34019f389faf0e8d8036fb3987`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1cd8c91ab257624a491a4e5e9eef35e0`
     );
     let data = await response.json();
     return data;
+  } catch (error) {
+    console.log(error);
   }
+}
   getWeatherLocation()
   let change = document.querySelector(".main__info__change")
   let mainInfo = document.querySelector(".main__info")
@@ -59,6 +64,7 @@ getWeather()
   let mainFind = document.querySelector(".main__find")
   let mainEror = document.querySelector(".main__eror")
   let tryAgainBtn = document.querySelector(".main__try")
+  
   change.addEventListener("click", (e) => {
     mainInfo.style.display = "none"
     mainInput.style.display = "flex"
@@ -66,38 +72,35 @@ getWeather()
 
   mainFind.addEventListener("click", async (e) => {
    let city = input.value.toLowerCase();
-   console.log(e.target.value)
+   console.log(input.value)
+   try {
     let weatherData = await getCity(city);
-    createWeather(weatherData)
-    mainInput.style.display = "none"
-    mainInfo.style.display = "flex"
-    if (!weatherData.main) { // проверяем наличие данных в объекте weatherData
+    if (!weatherData.main) {
+      // проверяем наличие данных в объекте weatherData
+      
       mainEror.style.display = "flex";
       mainInput.style.display = "none";
     } else {
-      createWeather(weatherData);
+      createWeather(weatherData)
       mainInput.style.display = "none";
       mainInfo.style.display = "flex";
       mainEror.style.display = "none";
     }
+  } catch (error) {
+    console.log(error.message);
+    mainEror.style.display = "flex";
+    mainInput.style.display = "none";
+  }
   } )
   tryAgainBtn.addEventListener("click", (e) => {
     mainEror.style.display = "none";
     mainInput.style.display = "flex"
-  });
+    input.value = "";
+   });
 
- 
-
-
-// const result = await getWeather()
-// console.log(result)
-// createWeather(result)
-// const resultTwo = await getIp()
-// console.log(resultTwo)
-// createWeather(resultTwo)
-function createWeather(obj){
-    let deg = document.querySelector(".main__info__deg")
+ function createWeather(obj){
+   let deg = document.querySelector(".main__info__deg")
     deg.innerText = obj.main.temp.toFixed() + "℃";
     let city = document.querySelector(".main__info__windy")
-    city.innerText = obj.weather[0].description + " in " + obj.name ;
+    city.innerText = obj.weather[0].description + " in " + obj.name
 }
